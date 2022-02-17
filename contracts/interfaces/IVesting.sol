0461837c5f5054fd0bb718097c67f840ef45f2c1
Richard Liang <richardliang2015@u.northwestern.edu>
2022-02-17 22:14:39 +0800

    Add collateral vault integration WIP

--- contracts/interfaces/IVesting.sol
+++ contracts/interfaces/IVesting.sol
@@ -2,12 +2,13 @@
 pragma solidity 0.6.10;
 
 interface IVesting {
-    function recipient() external;
-    function vestingAmount() external;
-    function vestingBegin() external;
-    function vestingCliff() external;
-    function vestingEnd() external;
-    function lastUpdate() external;
+    function recipient() external view returns(address);
+    function vestingToken() external view returns(address);
+    function vestingAmount() external view returns(uint256);
+    function vestingBegin() external view returns(uint256);
+    function vestingCliff() external view returns(uint256);
+    function vestingEnd() external view returns(uint256);
+    function lastUpdate() external view returns(uint256);
 
     function setRecipient(address recipient_) external;
     function claim() external;
