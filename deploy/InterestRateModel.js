e42365b3d468463338baadf3eb30674b9236d19b
Richard Liang <richardliang2015@u.northwestern.edu>
2022-03-20 19:05:07 +0800

    Fix interest rate model

--- deploy/InterestRateModel.js
+++ deploy/InterestRateModel.js
@@ -5,7 +5,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
   await deploy("WhitePaperInterestRateModel", {
     from: deployer,
-    args: ['1000000000000000000', '1000000000000000000'],
+    args: ['50000000000000000', '150000000000000000'],
     log: true,
     deterministicDeployment: false
   })
